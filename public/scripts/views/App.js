21ca836d44aa3aaa86a12e1b68f2b787326d1564
Shantanu Raj <s@sraj.me>
2015-06-28 05:12:17 +0530

    Display home page!

---
+++ public/scripts/views/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from './Banner';
+
+class App extends React.Component {
+	render() {
+		return (
+			<Banner />
+		);
+	}
+}
+
+export default App;
---
+++ public/scripts/views/Banner.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class Banner extends React.Component {
+	render() {
+		return (
+			<div className="banner">
+				Welcome to -
+			</div>
+		);
+	}
+}
+
+export default Banner;
