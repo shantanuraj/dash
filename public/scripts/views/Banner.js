3b4ca03b9223c59e08e3c732b8f7b133b1309d98
Shantanu Raj <s@sraj.me>
2015-06-28 05:59:46 +0530

    Add ugly looking CSS.

--- public/scripts/views/Banner.js
+++ public/scripts/views/Banner.js
@@ -1,13 +1,22 @@
-var React = require('react');
+import React from 'react';
 
 class Banner extends React.Component {
 	render() {
 		return (
-			<div className="banner">
-				Welcome to -
+			<div style={styles.banner}>
+				<p>Welcome to <img src="/images/dash.png" width={40} height={40}/></p>
 			</div>
 		);
 	}
 }
+
+var styles = {
+	banner: {
+		display: 'flex',
+		backgroundColor: '#065150',
+		color: 'white',
+		textAlign: 'center',
+	},
+};
 
 export default Banner;
